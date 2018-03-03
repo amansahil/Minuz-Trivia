fb47360053658af61dc4951892ecc2fbec3a7bc4
amansahil <33552991+amansahil@users.noreply.github.com>
2018-03-03 11:26:50 +0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 //Nice to see you here
 import React from 'react';
 import { View } from 'react-native';
-import Router from './Router'; //This manages the screnes switch to the file to learn more
+import Router from './Router'; //This manages the scenes, switch to the file to learn more
 import Play from './components/Play';
 
 const App = () => {
   return (
       <View style={{ flex: 1 }}>
-        <Router>// This tag is used on top of your initial screen , what is basically does switch the tag bettween it for navigation
+        <Router>// This tag is used on top of your initial screen , what it basically does is switch the tag bettween it for navigation
           <Play />// Intital screen , kind of
         </Router>
       </View>
